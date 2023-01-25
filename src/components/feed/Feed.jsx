dfa960c035af318435aee133afd3b7f4f535bbb6
mpastore90 <mpastore90@users.noreply.github.com>
2023-01-25 14:01:29 +0100

    Feed Post and Shere components

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,8 +1,15 @@
 import "./feed.css"
+import Share from "../share/Share"
+import Post from "../post/Post"
+
+
 export default function Feed() {
   return (
     <div className="feed">
-      feed
+      <div className="feedWrapper">
+        <Share/>
+        <Post/>
+      </div>      
     </div>
   )
 }
