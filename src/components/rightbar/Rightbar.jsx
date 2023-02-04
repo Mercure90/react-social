a6b8a9da7fc0ff13351794c7d2dba8505ee70c7f
mpastore90 <mpastore90@users.noreply.github.com>
2023-02-04 23:14:24 +0100

    login register and profile pages

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,11 +1,11 @@
 import "./rightbar.css"
 import { Users} from "../../dummyData";
 import Online from "../online/Online";
-export default function Rightbar() {
-  return (
-    <div className="rightbar">
-      <div className="rigtbarWrapper">
-        <div className="birthdayContainer">
+export default function Rightbar({profile}) {
+  const HomeRightBar = () => {
+    return(
+      <>
+      <div className="birthdayContainer">
           <img className="birthdayImg" src="assets/gift.png" alt="" />
           <span className="birthdayText">
             <b>Pola Foster</b> and <b>3 others friends</b> have their birthday today
@@ -18,6 +18,64 @@ export default function Rightbar() {
             <Online key={u.id} user={u}/>
           ))}
         </ul>
+      </>
+    )
+  }
+
+  const ProfileRightBar = () => {
+    return(
+      <> 
+      <h4 className="rightBarTitle">User Information</h4>
+      <div className="rightBarInfo">
+        <div className="rightBarInfoItem">
+          <span className="rightBarInfoKey">City: </span>
+          <span className="rightBarInfoValue">New York </span>
+        </div>
+        <div className="rightBarInfoItem">
+          <span className="rightBarInfoKey">From: </span>
+          <span className="rightBarInfoValue">Madrid </span>
+        </div>
+        <div className="rightBarInfoItem">
+          <span className="rightBarInfoKey">Relationship: </span>
+          <span className="rightBarInfoValue">Single </span>
+        </div>
+      </div>
+      <h4 className="rightBarTitle">User Friends</h4>
+      <div className="rightbarFollowings">
+        <div className="rightbarFollowing">
+          <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Carter</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Carter</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Carter</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Carter</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Carter</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Carter</span>
+        </div>
+      </div>
+
+      </>
+    )
+  }
+
+  return (
+    <div className="rightbar">
+      <div className="rigtbarWrapper">
+        {profile?<ProfileRightBar/>:<HomeRightBar/>}
       </div>      
     </div>
   )
---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,39 @@
+import "./profile.css";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Topbar from "../../components/topbar/Topbar";
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                src="assets/post/3.jpeg"
+                alt=""
+                className="profileCoverImg"
+              />
+              <img
+                src="assets/person/7.jpeg"
+                alt=""
+                className="profileUserImg"
+              />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">My name</h4>
+                <span className="profileInfoDesc">My DEcription</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
